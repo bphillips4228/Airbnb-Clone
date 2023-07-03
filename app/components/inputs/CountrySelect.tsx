d840c00b951ffa4f335c90240d3486da430a440f
bphillips4228 <bphillips4228@gmail.com>
2023-07-03 13:42:36 -0500

    1.1.2
    -syntax error corrected

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -11,12 +11,12 @@ export type CountrySelectValue = {
     value: string
 }
 
-interface CountrySelectProps {
+interface ectProps {
     value?: CountrySelectValue;
     onChange: (value: CountrySelectValue) => void;
 }
 
-const CountrySelect: React.FC<CountrySelectProps> = ({
+const ect: React.FC<ectProps> = ({
     value,
     onChange
 }) => {
@@ -32,33 +32,34 @@ const CountrySelect: React.FC<CountrySelectProps> = ({
                 value={value}
                 onChange={(value) => onChange(value as CountrySelectValue)}
                 formatOptionLabel={(option: any) => (
-                    <div className="flex flex-row items-center gap-3">
-                        <div>{option.flag}</div>
-                        <div>
-                            {option.label},
-                            <span className="text-neutral-500 ml-1">
-                                {option.region}
-                            </span>
-                        </div>
+                <div className="
+                flex flex-row items-center gap-3">
+                    <div>{option.flag}</div>
+                    <div>
+                    {option.label},
+                    <span className="text-neutral-500 ml-1">
+                        {option.region}
+                    </span>
                     </div>
+                </div>
                 )}
-                className={{
-                    control: () => 'p-3 border-2',
-                    input: () => 'text-lg',
-                    option: () => 'text-lg',
+                classNames={{
+                control: () => 'p-3 border-2',
+                input: () => 'text-lg',
+                option: () => 'text-lg'
                 }}
                 theme={(theme) => ({
-                    ... theme,
-                    borderRadius: 6,
-                    colors: {
-                        ... theme.colors,
-                        primary: 'black',
-                        primary25: '#ffe4e6'
-                    }
+                ...theme,
+                borderRadius: 6,
+                colors: {
+                    ...theme.colors,
+                    primary: 'black',
+                    primary25: '#ffe4e6'
+                }
                 })}
             />
         </div>
     );
 }
  
-export default CountrySelect;
+export default ect;
